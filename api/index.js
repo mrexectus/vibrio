c0ee706fd704243b15cc03237332ae25e534c729
mrexectus <mrexectus@gmail.com>
2025-11-29 21:31:41 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,38 +1,25 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    status: "OK",
-    message: "Vibrio API çalışıyor ✔",
-    time: new Date().toISOString()
-  });
-}
-import puppeteer from "puppeteer";
+import { chromium } from "@playwright/browser-chromium";
 
 export default async function handler(req, res) {
-
-  // HTML içerğini POST ile alıyoruz
   const { html } = req.body;
 
   if (!html) {
-    return res.status(400).json({ error: "HTML içeriği gönderilmedi." });
+    return res.status(400).json({ error: "HTML içeriği eksik." });
   }
 
   try {
-    const browser = await puppeteer.launch({
-      headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"]
-    });
-
+    const browser = await chromium.launch();
     const page = await browser.newPage();
-    await page.setContent(html, { waitUntil: "networkidle0" });
+    await page.setContent(html, { waitUntil: "networkidle" });
 
-    const pdfBuffer = await page.pdf({ format: "A4" });
+    const pdf = await page.pdf({ format: "A4" });
     await browser.close();
 
     res.setHeader("Content-Type", "application/pdf");
     res.setHeader("Content-Disposition", "attachment; filename=vibrio-report.pdf");
-    return res.send(pdfBuffer);
+    res.send(pdf);
 
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 }
